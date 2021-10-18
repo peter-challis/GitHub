7e203f9f860a1531895f3832b852c55fe30f5c5a
Peter-Challis <Peter.Challis@kyndryl.com>
2021-10-18 14:54:45 +0100

    Update CreateCopy.ps1

--- CreateCopy.ps1
+++ CreateCopy.ps1
@@ -7,5 +7,10 @@ else
 {
   write-host "folder already exists"
 }
-Copy-Item 'C:\working\DiskSpace.txt' 'C:\working\archive\DiskSpace.txt'
-ls C:\working\archive\DiskSpace.txt | ? { !$_.PSIsContainer   -and   $_.extension -eq '.txt' } |   ren -NewName { "$($_.BaseName) $(get-date -format "yyyymm%d%H%M%S")$($_.extension) "  }  
+#Copy-Item 'C:\working\DiskSpace.txt' 'C:\working\archive\DiskSpace.txt'
+#ls C:\working\archive\DiskSpace.txt | ? { !$_.PSIsContainer   -and   $_.extension -eq '.txt' } |   ren -NewName { "$($_.BaseName) $(get-date -format "yyyymm%d%H%M%S")$($_.extension) "  }
+
+if(Test-Path C:\working\DiskSpace.txt){
+$destination = "C:\working\archive\DiskSpace.txt"+[DateTime]::Now.ToString("yyyyMMdd-HHmmss")+".txt"
+Copy-Item -Path C:\working\archive\DiskSpace.txt -Destination $destination
+}
