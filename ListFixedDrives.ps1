b33a315916824068f9949bb7af9c520ed45dabfd
Peter-Challis <Peter.Challis@kyndryl.com>
2021-10-21 10:35:08 +0100

    Create ListFixedDrives.ps1

---
+++ ListFixedDrives.ps1
@@ -0,0 +1,5 @@
+﻿
+[System.IO.DriveInfo]::getdrives() | Where-Object {$_.DriveType -eq 'Fixed'} | export-csv -Path ‘c:\FixedDrives.csv’ -NoTypeInformation
+$users=Import-Csv C:\FixedDrives.csv
+$users[0].name | Out-file C:\FixedPassed.txt
+
