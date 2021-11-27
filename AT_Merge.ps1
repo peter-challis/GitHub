2d8380029e287fb02e8d31d72a313d6d345476e9
Peter-Challis <Peter.Challis@kyndryl.com>
2021-11-27 08:54:13 +0000

    d

---
+++ AT_Merge.ps1
@@ -0,0 +1,30 @@
+﻿New-Item -ItemType "directory" -Path "c:\Patching\Merge" -Force
+New-Item -ItemType "directory" -Path "c:\Patching\Report" -Force
+New-Item -ItemType "directory" -Path "c:\Patching\Archive" -Force
+
+get-childitem -Path "\\LAPTOP-T6UBO1E1\C`$\automation\*_installed.csv" |
+    where-object {$_.LastWriteTime -le (get-date).AddDays(-0)} | 
+    copy-item -destination C:\Patching\Report
+
+$pathin = 'c:\patching\report'
+$pathout = 'c:patching\Merge\merged.csv'
+$list = Get-ChildItem -Path $pathin | select FullName
+foreach($file in $list){
+    Import-Csv -Path $file.FullName | Export-Csv -Path $pathout -Append -NoTypeInformation
+}
+
+# Compress-Archive -Path C:\Patching\Merge\merged.csv -DestinationPath C:\Patching\Archive\*.zip
+# Get-ChildItem -Path C:\Patching\Merge\merged.csv |
+#   Compress-Archive -DestinationPath C:\Patching\Archive\"PatchArchive-$((Get-Date).ToString('yyyy-MM-dd-HH-mm-ss'))".zip
+# Remove-Item C:\Patching\report\merged.csv
+
+# $Folder = "C:\Patching\Report"
+# Get-ChildItem $Folder -Recurse -Force -ea 0 |
+# ? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-100)} |
+# ForEach-Object {
+#   $_ | del -Force
+#   $_.FullName | Out-File C:\Patching\deletedlog.txt -Append
+# }
+
+
+Copy-Item C:\Patching\Archive\Patch*.zip -Destination \\LAPTOP-T6UBO1E1\C`$\automation
--- AT_Patching_MergeExcel.ps1
+++ AT_Patching_MergeExcel.ps1
@@ -1,18 +1,25 @@
-﻿# Create new folder and over write if exists
-New-Item -Path c:\PostPatching -ItemType directory -force
+﻿# Create new folders and over write if exists
+# New-Item -Path c:\PostPatching -ItemType directory -force
+
+New-Item -ItemType "directory" -Path "c:\PostPatching" -force
+New-Item -ItemType "directory" -Path "c:\PostPatching\Reports"
+New-Item -Path "c:\PostPatching" -Name "archive" -ItemType "directory"
 
 # Create date stamp folder for imports from endpoints
-$path = "InstalledPatches-$((Get-Date).ToString('yyyy-MM-dd-HH-mm-ss'))"
-New-Item -ItemType Directory -Path $path
-$path
-# copy <servername>_installed.csv from remote SFS server to date stamped folder
-# possibly only copy files with a date stamp of x
+# $path = "InstalledPatches-$((Get-Date).ToString('yyyy-MM-dd-HH-mm-ss'))"
+# New-Item -ItemType Directory -Path $path
+# New-Item -ItemType "directory" -Path c:\PostPatching\"$path"
+
+# Note the patching report will have the same name each month.  That file will be pulled from the endpoint on to the SFS, overwritting last months csv
+
+# copy *_installed.csv file no older than x days from remote SFS server to csv rep on WSUS
 get-childitem -Path "\\LAPTOP-T6UBO1E1\C`$\automation" |
-    where-object {$_.LastWriteTime -lt (get-date).AddDays(-2)} | 
-    copy-item -destination $path
+    where-object {$_.LastWriteTime -le (get-date).AddDays(-10)} | 
+    copy-item -destination c:\PostPatching
 
 # merge all .csv files into one
-Get-ChildItem -Filter $path\*.csv | Select-Object -ExpandProperty FullName | Import-Csv | Export-Csv .\"CombinedPatches-$((Get-Date).ToString('yyyy-MM-dd-HH-mm-ss'))".csv -NoTypeInformation -Append
+# Get-ChildItem -Filter $path\*.csv | Select-Object -ExpandProperty FullName | Import-Csv | Export-Csv .\"CombinedPatches-$((Get-Date).ToString('yyyy-MM-dd-HH-mm-ss'))".csv -NoTypeInformation -Append
+Get-ChildItem -Filter c:\PostPatching\*.csv | Select-Object -ExpandProperty FullName | Import-Csv | Export-Csv .\"CombinedPatches-$((Get-Date).ToString('yyyy-MM-dd-HH-mm-ss'))".csv -NoTypeInformation -Append
 
 # Compress single file
 Compress-Archive -Path C:\PostPatching\Combinded*.csv -DestinationPath C:\PostPatching\*.zip
@@ -25,7 +32,7 @@ Remove-Item C:\PostPatching\*.csv
 #Delete files older than 6 months
 $Folder = "C:\PostPatching"
 Get-ChildItem $Folder -Recurse -Force -ea 0 |
-? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-30)} |
+? {!$_.PsIsContainer -and $_.LastWriteTime -lt (Get-Date).AddDays(-10)} |
 ForEach-Object {
    $_ | del -Force
    $_.FullName | Out-File C:\PostPatching\deletedlog.txt -Append
